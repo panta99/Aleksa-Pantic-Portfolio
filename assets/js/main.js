b4380badb55e45126ac4353de05829af6d73ba77
Aleksa Pantic <77241244+panta99@users.noreply.github.com>
2022-01-21 23:16:32 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,8 +14,8 @@ let meniispis = `<div class="container">
                     <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">`;
 
-nizLinkova = ["aboutme","skills","projects","contact"];
-nizMeni = ["About me","Skills","Projects","Contact"];
+nizLinkova = ["aboutme","services","skills","projects","contact"];
+nizMeni = ["About me","Services","Skills","Projects","Contact"];
 
 for(let i=0;i<nizLinkova.length;i++){
     meniispis += `<li class="nav-item ${nizLinkova[i]}"><a class="nav-link bojamenija" href="#${nizLinkova[i]}">${nizMeni[i]}</a></li>`
@@ -91,7 +91,7 @@ function skillsIspis(){
         ispis += ` <li>
                     <h3 class="fw-bold">${nizNaziv[i]}</h3><span class="linija"><span  id="${idNaziv[i]}"  class=""></span></span>
                     </li>`
-                   
+                    
     }
     skills.innerHTML = ispis;
 }
@@ -131,20 +131,27 @@ window.addEventListener("scroll", ()=>{
         brojac1++;
     }
    
-})
+});
 
-//Pojava elemenata na scroll
+
+//Pojava projekat i services elemenata na scroll
 let sekcije2 = document.querySelectorAll("section");
 let brojac2 = 0;
-let promena;
+let brojac3 = 0;
 
 window.addEventListener("scroll", ()=>{
     let trenutna = "";
-    function dodeladruge(){
-        document.querySelector("#projekti2").classList.add("pojavljivanje")
+    function dodelaprojekta2(){
+        document.querySelector("#projekti2").classList.add("pojavljivanje");
     }
-    function dodelatrece(){
-        document.querySelector("#projekti3").classList.add("pojavljivanje")
+    function dodelaprojekta3(){
+        document.querySelector("#projekti3").classList.add("pojavljivanje");
+    }
+    function dodelaservices2(){
+        document.querySelector(".services2").classList.add("pojavljivanje");
+    }
+    function dodelaservices3(){
+        document.querySelector(".services3").classList.add("pojavljivanje");
     }
     sekcije2.forEach( sekcija => {
         let vrhSekcije1 = sekcija.offsetTop;
@@ -154,59 +161,74 @@ window.addEventListener("scroll", ()=>{
         }
     })
     if((trenutna == "projects") && (brojac2 == 0)){
-        document.querySelector("#projekti1").classList.add("pojavljivanje")
-        setTimeout(dodeladruge,700)
-        setTimeout(dodelatrece,1400)
-        brojac1++;
+        document.querySelector("#projekti1").classList.add("pojavljivanje");
+        setTimeout(dodelaprojekta2,700);
+        setTimeout(dodelaprojekta3,1400);
+        brojac2++;
+    }
+    else if((trenutna == "services") && (brojac3 == 0)){
+        document.querySelector(".services1").classList.add("pojavljivanje");
+        setTimeout(dodelaservices2,700);
+        setTimeout(dodelaservices3,1400);
+        brojac3++;
     }
    
 })
 
 //Validacija forme
+var tacnostForme = false;
 $(document).ready(function(){
     $("#posaljiPodatke").click(proveriFormu);
+    $("#ime").blur(proveriIme);
+    $("#prezime").blur(proveriPrezime);
+    $("#mejl").blur(proveriMejl);
+    $("#biraj").blur(proveriOdabir);
+    $("#txtArea").blur(proveriPoruku);
+    
 })
 
-function proveriFormu(e){
-    e.preventDefault();
-
-    var tacnostForme = true;
-
-    var ime = document.getElementById("ime").value;
-    var prezime = document.getElementById("prezime").value;
-    var mejl = document.getElementById("mejl").value;
-    var message = document.getElementById("txtArea").value;
-
-    var imeGr = document.getElementById("greskaNaImenu");
-    var prezimeGr = document.getElementById("greskaNaPrezimenu");
-    var mejlGr = document.getElementById("greskaNaMejlu");
-    var tekstPorukaGr = document.getElementById("greskaNaTekstPoruka");
-
-    var regularniIme = /^[A-ZČĆŠĐŽ][a-zčćžđš]{2,15}(\s[A-ZČĆŠĐŽ][a-zčćžđš]{2,15}){0,1}$/;
-    var regularniPrezime = /^[A-ZČĆŠĐŽ][a-zčćžđš]{2,15}(\s[A-ZČĆŠĐŽ][a-zčćžđš]{2,15}){0,1}$/;
-    var regularniMejl = /^[a-zčćžđš][a-z\d\-\.\wčćžđš]+\@[a-z]+(\.[a-z]{2,12}){1,2}$/
-    var regularniMessage = /^[A-z\.\,\s\w\d\t\nčćžđš\/\@]+$/
-
-    function pokaziGresku(promenljiva, izraz, idPoruke){
-
-        if (!izraz.test(promenljiva)){
-            tacnostForme = false;
-            idPoruke.style.visibility = "visible";
-        }
-
-        else {
-            idPoruke.style.visibility = "hidden";
-        }
-
+function pokaziGresku(promenljiva, izraz, idPoruke,greska){
+    if(promenljiva=="")
+    {
+        idPoruke.textContent="You can't leave the field blank";
+        tacnostForme = false;
+        idPoruke.style.visibility = "visible";
+    }
+    else if (!izraz.test(promenljiva)){
+        idPoruke.textContent=greska;
+        tacnostForme = false;
+        idPoruke.style.visibility = "visible";
     }
 
-    pokaziGresku(ime, regularniIme, imeGr);
-    pokaziGresku(prezime, regularniPrezime, prezimeGr);
-    pokaziGresku(mejl, regularniMejl, mejlGr);
-    pokaziGresku(message, regularniMessage, tekstPorukaGr);
+    else {
+        idPoruke.style.visibility = "hidden";
+        tacnostForme = true;
+    }
 
+}
+function proveriIme(){
+    var ime = document.getElementById("ime").value;
+    var imeGr = document.getElementById("greskaNaImenu");
+    var greskaIme = "Name must begin with capital letter";
+    var regularniIme = /^[A-ZČĆŠĐŽ][a-zčćžđš]{2,15}(\s[A-ZČĆŠĐŽ][a-zčćžđš]{2,15}){0,1}$/;
+    pokaziGresku(ime, regularniIme, imeGr,greskaIme);
+}
+function proveriPrezime(){
+    var prezime = document.getElementById("prezime").value;
+    var prezimeGr = document.getElementById("greskaNaPrezimenu");
+    var greskaPrezime ="Last name must begin with capital letter";
+    var regularniPrezime = /^[A-ZČĆŠĐŽ][a-zčćžđš]{2,15}(\s[A-ZČĆŠĐŽ][a-zčćžđš]{2,15}){0,1}$/;
+    pokaziGresku(prezime, regularniPrezime, prezimeGr,greskaPrezime);
+}
+function proveriMejl(){
+    var mejl = document.getElementById("mejl").value;
+    var mejlGr = document.getElementById("greskaNaMejlu");
+    var greskamejl = "Mail in wrong format, example : example@example.example";
+    var regularniMejl = /^[a-zčćžđš][a-z\d\-\.\wčćžđš]+\@[a-z]+(\.[a-z]{2,12}){1,2}$/;
+    pokaziGresku(mejl, regularniMejl, mejlGr,greskamejl);
+}
+function proveriOdabir(){
     var izbor = document.getElementById("biraj").value;
-
     if(izbor == "0"){
         tacnostForme = false;
         document.getElementById("greskaNaIzboru").style.visibility = "visible";
@@ -214,7 +236,24 @@ function proveriFormu(e){
 
     else{
         document.getElementById("greskaNaIzboru").style.visibility = "hidden";
+        tacnostForme = true;
     }
+}
+function proveriPoruku(){
+    var message = document.getElementById("txtArea").value;
+    var tekstPorukaGr = document.getElementById("greskaNaTekstPoruka");
+    var regularniMessage = /^[A-z\.\,\s\w\d\t\nčćžđš\/\@]+$/
+    var greskaPoruka ="You can't leave field blank";
+    pokaziGresku(message, regularniMessage, tekstPorukaGr,greskaPoruka);
+}
+function proveriFormu(e){
+    e.preventDefault();
+
+    proveriIme();
+    proveriPrezime();
+    proveriMejl();
+    proveriOdabir();
+    proveriPoruku();
 
     if(tacnostForme == true){
         document.getElementById("nijeGreska").style.visibility = "visible";
@@ -223,6 +262,5 @@ function proveriFormu(e){
     else{
         document.getElementById("nijeGreska").style.visibility = "hidden";
     }
-
-
 }
+
